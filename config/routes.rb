989e65c9435a114e95e34d397dce9edeef35f34b
Sid Feygin <sid.feygin@gmail.com>
2013-01-04 14:15:40 -0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   #match '/', to: 'static_pages#home' <- not idiomatic
   root to: 'static_pages#home'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -92,6 +92,16 @@ describe User do
     it { should be_invalid }
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM"}
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
   describe "return value of authenticate method" do
     before { @user.save }
     let(:found_user) { User.find_by_email(@user.email) }
@@ -107,4 +117,6 @@ describe User do
       specify { user_for_invalid_password.should be_false }
     end
   end
+
+
 end
