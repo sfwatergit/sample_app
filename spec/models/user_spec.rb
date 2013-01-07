3bf1e1a72816393ef4580532e6fcaa5b1deb0c6a
Sid Feygin <sid.feygin@gmail.com>
2013-01-07 14:15:58 -0800

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,27 +15,37 @@ describe User do
 
   before { @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
 
-    subject { @user }
+  subject { @user }
 
-    it { should respond_to(:name)}
-    it { should respond_to(:email)}
-    it { should respond_to(:password_digest) }
-    it { should respond_to(:password) }
-    it { should respond_to(:password_confirmation) }
-    it {  should respond_to(:remember_token) }
-    it { should respond_to(:authenticate) }
-    it { should respond_to(:authenticate) }
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
 
-    it { should be_valid }
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
   describe "when name is not present" do
-    before { @user.name = " "}
+    before { @user.name = " " }
     it { should_not be_valid }
   end
 
   describe "when email is not present" do
     before { @user.email = " " }
-    it { should_not be_valid}
+    it { should_not be_valid }
   end
 
   describe "when name is too long" do
@@ -71,7 +81,7 @@ describe User do
       user_with_same_email.save
     end
 
-    it{ should_not be_valid }
+    it { should_not be_valid }
   end
 
   describe "when password is not present" do
@@ -95,7 +105,7 @@ describe User do
   end
 
   describe "email address with mixed case" do
-    let(:mixed_case_email) { "Foo@ExAMPle.CoM"}
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
 
     it "should be saved as all lower-case" do
       @user.email = mixed_case_email
